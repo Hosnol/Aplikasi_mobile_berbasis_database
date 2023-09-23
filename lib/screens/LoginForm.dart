4b1a36b9ad81ce04945f96d54f7632d17a25816a
Hosnol Arifin <hosnolarifin220@gmail.com>
2023-09-23 14:53:19 +0700

    tampilan login

---
+++ lib/screens/LoginForm.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool _isPasswordObscure = true; // Untuk mengontrol apakah teks kata sandi harus disembunyikan
+  bool _isPasswordFilled = false; // Untuk mengontrol apakah field kata sandi sudah diisi
+
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Cashbook'),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 50.0),
+                Text(
+                  'Selamat Datang di',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 20.0,
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Image.asset(
+                  "assets/images/cashbook-logo.png",
+                  width: 250.0,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      prefixIcon: Icon(Icons.person),
+                      hintText: 'Username',
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: _isPasswordObscure,
+                    onChanged: (value) {
+                      setState(() {
+                        // Tampilkan tombol "Lihat Password" hanya jika field kata sandi diisi
+                        _isPasswordFilled = value.isNotEmpty;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      prefixIcon: Icon(Icons.vpn_key),
+                      hintText: 'Password',
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                      suffixIcon: _isPasswordFilled
+                          ? IconButton(
+                        icon: Icon(_isPasswordObscure
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordObscure = !_isPasswordObscure;
+                          });
+                        },
+                      )
+                          : null, // Tambahkan null jika field kata sandi kosong
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(30.0),
+                  width: double.infinity,
+                  child: FlatButton(
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {},
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20.0)
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
